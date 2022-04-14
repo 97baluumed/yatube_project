3f86a9d01842ddf916bdbfc992e96b950164c6fa
Новожилов Максим <notolerances@yandex.ru>
2022-04-14 18:59:12 +0300

    надеюсь, что будет работать

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    html_content = '<html><head><title>yatube - соц. сеть для блогеров</title></head><body>'
+    html_content += '<h1>Главная страница</h1>'
+    html_content += '</body></html>'    
+    return HttpResponse(html_content) 
 
 def group_posts(request):
     return HttpResponse('Посты, отфильтрованные по группам')
