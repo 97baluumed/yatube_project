6e3263c9ff8b111abb5010a24ec1abaf056ac035
Новожилов Максим <notolerances@yandex.ru>
2022-04-16 21:21:08 +0300

    Файл group_list не хочет прикрепляться к ссылке и не работает по аналогии с index. очень странно все это

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'Yatube'
+
 urlpatterns = [
-    path('', views.index),
-    path('posts/', views.group_posts),
-    path('posts/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('posts/', views.group_posts, name='group'),
+    path('group/<slug:slug>', views.group_posts, name='group_list'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html_content = '<html><head><title>yatube - соц. сеть для блогеров</title></head><body>'
-    html_content += '<h1>Главная страница</h1>'
-    html_content += '</body></html>'    
-    return HttpResponse(html_content) 
+    template = 'posts/index.html'
+    title = "Yatube"
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context) 
 
-def group_posts(request):
-    return HttpResponse('Посты, отфильтрованные по группам')
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group_text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'gr_text': group_text,
+    }
+    return render(request, template, context)
